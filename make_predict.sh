a66a5274d9ea1bd947fa76e53423b4e180ae0ccb
mgblackwater <mgblackwater@gmail.com>
2020-11-08 10:57:17 +0800

    Update make_predict.sh

--- make_predict.sh
+++ make_predict.sh
@@ -25,4 +25,4 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST http://localhost:$PORT/predict
+     -X POST http://mgblackwater-flask-ml-service.azurewebsites.net/predict
